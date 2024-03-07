2fc85ec28676f929da06798eb66ad54f09fb7380
argecyplusplus <tema.chernyuk@gmail.com>
2024-03-07 15:23:54 +0300

    update

---
+++ src/main/java/ru/chernyukai/projects/lab1jokebot/Lab1JokebotApplication.java
@@ -0,0 +1,13 @@
+package ru.chernyukai.projects.lab1jokebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1JokebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1JokebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/chernyukai/projects/lab1jokebot/Lab1JokebotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chernyukai.projects.lab1jokebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1JokebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
